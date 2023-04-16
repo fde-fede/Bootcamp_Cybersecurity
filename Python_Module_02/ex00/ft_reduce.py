9f6927486b6c10e2511e92b513ed73e35dcb4213
Felix De federico bedoya <fde-fede@c2r6s2.42malaga.com>
2023-04-16 17:58:06 +0200

    M_02_ex02

--- Python_Module_02/ex00/ft_reduce.py
+++ Python_Module_02/ex00/ft_reduce.py
@@ -1,1 +1,21 @@
+from collections.abc import Iterable
+
 def ft_reduce(function_to_apply, iterable):
+    """Apply function of two arguments cumulatively.
+    Args:
+        function_to_apply: a function taking an iterable.
+        iterable: an iterable object (list, tuple, iterator).
+    Return:
+        A value, of same type of elements in the iterable parameter.
+        None if the iterable can not be used by the function.
+    """
+    if not isinstance(iterable, Iterable):
+        raise TypeError("Expected an iterable")
+    if not iterable:
+        raise TypeError("reduce() of empty sequence with no initial value")
+    for i, elem in enumerate(iterable):
+        if i:
+            reduced = function_to_apply(reduced, elem)
+        else:
+            reduced = elem
+    return reduced
---
+++ Python_Module_02/ex02/logger.py
@@ -0,0 +1,48 @@
+import time
+from random import randint
+import os
+
+def log(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        xtime = time.perf_counter() - start
+        with open("machine.log", mode = "a+", encoding="utf-8") as file:
+            user = os.getenv('USER', "unknown")
+            name = func.__name__.replace('_', ' ').title()
+            if xtime > 0.001:
+                file.write(f"({user}) Running")
+
+
+class CoffeeMachine():
+    water_level = 100
+    @log
+    def start_machine(self):
+        if self.water_level > 20:
+            return True
+        else:
+            print("Please add water!")
+            return False
+    @log
+    def boil_water(self):
+        return "boiling..."
+    @log
+    def make_coffee(self):
+        if self.start_machine():
+            for _ in range(20):
+                time.sleep(0.1)
+                self.water_level -= 1
+            print(self.boil_water())
+            print("Coffee is ready!")
+    @log
+    def add_water(self, water_level):
+        time.sleep(randint(1, 5))
+        self.water_level += water_level
+        print("Blub blub blub...")
+
+if __name__ == "__main__":
+    machine = CoffeeMachine()
+    for i in range(0, 5):
+        machine.make_coffee()
+    machine.make_coffee()
+    machine.add_water(70)

c385e0804b0cbd585aa9181e45f45e54a4782ae8
Felix De federico bedoya <fde-fede@c2r5s3.42malaga.com>
2023-04-20 16:13:26 +0200

    arach_cmplt/otp_init

--- arachnida/test.py
+++
@@ -1,8 +0,0 @@
-r = requests.get(image_url, stream=True)
-                    nombre_archivo = os.path.basename(image_url)
-                    try:
-                        with open(os.path.join(base_path, nombre_archivo), 'wb') as f:
-                            r.raw.decode_content = True
-                            shutil.copyfileobj(r.raw, f)
-                    except Exception as e:
-                        print(f"Error: {e}")
